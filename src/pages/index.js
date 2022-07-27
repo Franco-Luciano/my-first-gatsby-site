0e1b1ed1ff3a674db98481746c05ace2f655cb73
Franco-Carlomagno <franco.carlomagno@acct.global>
2022-07-27 14:34:56 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt='an image of a black hole' src='https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2022/01/19/16425908838658.jpg'/>
     </Layout>
   )
 }
